c56c04f7ed3dabd6c72d139f1c9d8841ec74bb45
DaxiangN <daxiangna@gmail.com>
2022-01-30 16:26:30 -0500

    update

---
+++ Lesson_1_notes.py
@@ -0,0 +1,63 @@
+# Notes for Unit 1: Python Basics
+
+float(5)
+
+type(3.5)
+
+type(5)
+
+3+2
+
+print(3+2)
+
+3.0 + 3
+
+5 // 2
+
+5 / 2
+
+# two * gives the power, while one * is multiply
+
+3 ** 3
+
+3*5+1
+
+3*(5+1)
+
+10 / 3
+
+10.0 / 3.0
+
+2.1 ** 2.0
+
+2.2 * 3.0
+
+radius = 1
+
+#short hand to plus 1 to the value itself
+radius += 1
+radius
+
+a = 5.0
+a = a + 1.0
+
+x = int(input('Enter an integer:'))
+if x%2 == 0: 
+    print('')
+    print('Even')
+else:
+    print('')
+    print('Odd')
+print('Done with conditional')
+
+not True and False
+
+not (True and False)
+
+4 > + 4
+
+2 + 2 == 4
+
+# For comparison, Python promotes both to floating point.
+5 * 2 == 5.0 * 2.0
+# Python returns True
